3936fbe90133b2461fb73e54bdd5c296ec36edd4
muziyan <997432833@qq.com>
2021-09-20 17:09:30 +0800

    fix: list and get 通用地址错误

--- day_1_node/src/server/src/framework/router.js
+++ day_1_node/src/server/src/framework/router.js
@@ -4,8 +4,8 @@ const {
   init,get,create,update,del,list
 } = require("./api")
 
-router.get("/api/:list/:id",init,list);
-router.get("/api/:list",init,get);
+router.get("/api/:list/:id",init,get);
+router.get("/api/:list",init,list);
 router.post("/api/:list",init,create);
 router.put("/api/:list/:id",init,update);
 router.delete("/api/:list/:id",init,del);
